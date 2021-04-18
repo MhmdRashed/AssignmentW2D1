3ebabff9336ea219507fb28b0629d4c894a23bfd
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-04-19 02:25:57 +0300

    initial commit test

---
+++ AssignmentW2D1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssignmentW2D1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
