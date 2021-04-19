116fb2c3be0b818554364dd938ce1266af5ce130
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-04-19 04:17:12 +0300

    JSON Builder

--- AssignmentW2D1/Program.cs
+++ AssignmentW2D1/Program.cs
@@ -4,9 +4,46 @@ namespace AssignmentW2D1
 {
     class Program
     {
+        class JSONBuilder
+        {
+            private string html;
+
+            public JSONBuilder()
+            {
+                this.html = "";
+            }
+            public JSONBuilder beginBracket(string value)
+            {
+                this.html += "{\"" + value + "\":" ;
+                return this;
+            }
+            public JSONBuilder setContent(string value)
+            {
+                this.html += "\"" + value;
+                return this;
+            }
+            public JSONBuilder endBracket()
+            {
+                this.html += "\"}";
+                return this;
+            }
+            public string getHTML(string value)
+            {
+                return this.html;
+            }
+        }
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            JSONBuilder obj = new JSONBuilder();
+            obj.beginBracket("name").setContent("Mohammed").endBracket();
+            string res = obj.getHTML("FI");
+            Console.WriteLine(res);
         }
     }
 }
