eaf3b95fd69ee024c9ab692d400da232d53c74cd
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-04-19 05:45:24 +0300

    match

--- AssignmentW2D1/Program.cs
+++ AssignmentW2D1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 
 namespace AssignmentW2D1
 {
     class Program
     {
+
         class JSONBuilder
         {
             private string html;
@@ -27,10 +29,51 @@ namespace AssignmentW2D1
                 this.html += "\"}";
                 return this;
             }
-            public string getHTML(string value)
+            public string getHTML()
             {
                 return this.html;
             }
+        }
+
+
+        public static bool isMatched(string numbers)
+        {
+            int len = numbers.Length;
+
+            Stack stk = new Stack();
+
+            char tmp;
+
+            if(numbers.Length % 2 == 0)
+            {
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+
+                    stk.Push(numbers[i]);
+                    tmp = (char)stk.Peek();
+
+                    if (tmp == numbers[i])
+                    {
+                        stk.Pop();
+                    }
+                    else if (numbers[i] != tmp)
+                    {
+                        stk.Push(numbers[i]);
+                    }
+                    
+                }
+
+
+                Console.WriteLine("ANS: " + stk.Count); 
+                if (stk.Count == 0) { return true; }
+                else { return false; }
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
 
@@ -42,8 +85,19 @@ namespace AssignmentW2D1
         {
             JSONBuilder obj = new JSONBuilder();
             obj.beginBracket("name").setContent("Mohammed").endBracket();
-            string res = obj.getHTML("FI");
+            string res = obj.getHTML();
             Console.WriteLine(res);
+
+            //=========
+
+            string numbers = "12345678";
+            string numbers2 = "123321";
+            string numbers3 = "14667741";
+            Console.WriteLine(numbers + "\t" + isMatched(numbers)); ;
+            Console.WriteLine(numbers2 + "\t" + isMatched(numbers)); ;
+            Console.WriteLine(numbers3 + "\t" + isMatched(numbers)); ;
+
+
         }
     }
 }
